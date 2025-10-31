b6ed20c50fb69b783d1fa2b91325694e970ac50c
root <root@v2202510305603389692.megasrv.de>
2025-10-31 11:49:25 +0100

    from 06 to 02

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -59,7 +59,7 @@ export default function TestPage() {
               key={activeSection}
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6 }}
+              transition={{ duration: 0.2 }}
             >
               {renderSectionContent()}
             </motion.div>
